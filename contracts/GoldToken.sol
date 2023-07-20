a5b0eb293fe724c16bfabe4880355e898d1e5937
Manu Kapoor <manu.web3.0@gmail.com>
2023-07-20 01:35:21 -0600

    2nd - revised mint()

--- contracts/GoldToken.sol
+++ contracts/GoldToken.sol
@@ -2,29 +2,41 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+// OZ-ownable.sol: ADD - VS Code
 import "docs.chain.link/samples/DataFeeds/ReserveConsumerV3.sol";
 
 contract GoldToken is ERC20 {
 
     event TGOLDMinted(address indexed account, uint256 amount);
+    event GoldTestPassed(address indexed account, uint16 qtyGold, uint8 purityGold);
 
-    struct OwnerData {
-        string s_ownerName;
-        uint256 s_ownerIDNumber;
-        uint16 s_qtyGold;    // can only be either 10g, 100g or 1000g Gold bars, 24K purity only (99.9%)
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Unauthorized Account");
+        _;
     }
 
-    mapping(address => OwnerData) private s_userData;
+    address private owner;
 
-    string private constant GOLDPURITY = "24K";
+    struct UserData {
+        string s_userName;
+        uint256 s_userIDNumber;
+        uint16 s_qtyGold;       // can only be either 10g, 100g or 1000g Gold bars, 24K purity only (99.9%)
+        uint8 s_purityGold;     // an integer value reresenting Karat
+        uint8 s_flag;           // set to 1 when Gold-tests passed else default 0
+    }
+
+    mapping(address => UserData) private s_userData;
+
+    uint8 public constant GOLDPURITY = 24;
      
-    // ADD- 2*REQUIRES to check these 2 qty. and purity  
     // ADD- use this while calculating number of TGOLD tokens for user's Gold bar: ((s_qtyGold * 999) * 10**8) / 1000;
 
     GoldReserveAndPrice internal immutable s_goldReserveAndPrice;
 
     constructor () ERC20 ("GoldToken", "TGOLD") { 
-        s_goldReserveAndPrice = GoldReserveAndPrice(0x0813d4a158d06784FDB48323344896B2B1aa0F85);
+        owner = msg.sender;
+        s_goldReserveAndPrice = GoldReserveAndPrice(0xEc29164D68c4992cEdd1D386118A47143fdcF142);
         // VS CODE - dynamically store the above deployment-address in VS Code via a script
     }
 
@@ -32,27 +44,44 @@ contract GoldToken is ERC20 {
         return 8;
     }
 
-    function mint(address account, uint256 amount) public {
+    function testGoldBarPassed(address account) public returns (bool) {
+        require(s_userData[account].s_qtyGold == 10 || 
+                s_userData[account].s_qtyGold == 100 || 
+                s_userData[account].s_qtyGold == 1000, "Non-standard weight");
+
+        require(s_userData[account].s_purityGold == GOLDPURITY, "Non-standard purity");
+        
+        // set flag to 1
+        s_userData[account].s_flag = 1;
+        emit GoldTestPassed(account, s_userData[account].s_qtyGold, s_userData[account].s_purityGold);
+        return true;
+    }
+    
+    // Only Contract-Owner can mint all the TGOLD tokens to the user
+    function mint(address account) public onlyOwner() {
+        require(s_userData[account].s_flag == 1, "User's Gold-test not passed");
+
         uint256 totalSupply = totalSupply();
-        uint256 supplyAfterMint = totalSupply + amount;
-        require(supplyAfterMint <= uint256(s_goldReserveAndPrice.getLatestReserve()), "Exceeded reserves, reverted");
-        _mint(account, amount);
-        emit TGOLDMinted(account, amount);
+        uint256 amountToMint = ((s_userData[account].s_qtyGold * 999) * 10**8) / 1000;  // decimal of TGOLD is 8, purity 24K = 99.9%
+        uint256 supplyAfterMint = totalSupply + amountToMint;
+
+        require(supplyAfterMint <= uint256(s_goldReserveAndPrice.getLatestReserve()), "Exceeded reserves");
+
+        _mint(account, amountToMint);
+        emit TGOLDMinted(account, amountToMint);  // can add account.name instead of account.address in the event
     }
 
-    function testGoldBarPassed(uint16 goldQty, string memory goldPurity) public view returns (bool) {
-        require(
-            
-        )
-    }
-
-    function getOwnerGoldPrice(address account) public view returns (uint256) {
+    function getUserGoldPrice(address account) public view returns (uint256) {
         uint256 priceGold = uint256(s_goldReserveAndPrice.getLatestPrice());
         return ((uint256(s_userData[account].s_qtyGold) * 999) * priceGold) / 1000;
 }
 
-    function getOwnerData(address account) public view returns (string memory, uint256, uint16) {
-        return (s_userData[account].s_ownerName, s_userData[account].s_ownerIDNumber, s_userData[account].s_qtyGold);
+    function getUserData(address account) public view returns (string memory, uint256, uint16) {
+        return (s_userData[account].s_userName, s_userData[account].s_userIDNumber, s_userData[account].s_qtyGold);
+    }
+
+    function getContractOwner() public view returns(address) {
+        return owner;
     }
     
     // function getOwnerName(address account) public view returns (string memory) {
